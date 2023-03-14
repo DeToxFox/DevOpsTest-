5fa403adb6dd87f7a2c5f6c494da54380cb59aec
David Turner <95373983+DeToxFox@users.noreply.github.com>
2023-03-13 22:08:22 -0230

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,17 @@
 
+public class Test {
+  private String message;
+
+  public Test(String message) {
+    this.message = message;
+  }
+
+  public void theMessage() {
+    System.out.println(message);
+  }
+
+  public static void main(String[] args) {
+    Test test = new Test("Hello, world!");
+    test.theMessage();
+  }
+}
